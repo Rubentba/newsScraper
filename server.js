6d0e6a572c06cdaa85f2fbad99be55896970b483
Rubentba <rubenatorresbeltran@gmail.com>
2019-07-17 05:26:59 -0400

    Added changes made to server.js file. Morgan npm package not working.

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+// const logger = require('morgan')
+
+const PORT = process.env.PORT || 3000
+const app = express()
+
+// app.use(logger('dev'))
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static('public'))
+
+app.engine(
+    'hbs',
+    exphbs({
+        defaultLayout: 'main'
+    })
+)
+app.set('view engine', 'hbs')
+
+// require('./app/routes/htmlRoutes')(app)
+// require('./app/routes/apiRoutes')(app)
+
+
+app.listen(PORT, function () {
+    console.log('App running on port ' + PORT + '!')
+})
+
+module.exports = app
