b05ab063c4f3d552819b6be4e672e0f2618bcc83
Rubentba <rubenatorresbeltran@gmail.com>
2019-07-18 18:12:26 -0400

    Updated server to displayh handlbar files.

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+// Dependencies
 const express = require('express')
 const exphbs = require('express-handlebars')
 // const logger = require('morgan')
+const path = require('path')
 
 
+// For Heroku
 const PORT = process.env.PORT || 3000
 const app = express()
 
@@ -15,19 +18,21 @@ app.use(express.json())
 app.use(express.static('public'))
 
 
-app.engine(
-    'hbs',
-    exphbs({
-        defaultLayout: 'main'
-    })
-)
+// For Handlebars
+app.engine('hbs', exphbs({
+    defaultLayout: "main",
+    extname: '.hbs'
+}))
 app.set('view engine', 'hbs')
+app.set("views", path.join(__dirname, "app/views"))
 
 
+// Routes
 require('./app/routes/htmlRoutes')(app)
 require('./app/routes/apiRoutes')(app)
 
 
+// Making sure the server is live.
 app.listen(PORT, function () {
     console.log(`App running on port ${PORT}!`)
 })
