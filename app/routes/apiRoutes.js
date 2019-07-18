980b80b845b9fe58cf7ed32acbfde62839e49f8e
Rubentba <rubenatorresbeltran@gmail.com>
2019-07-18 05:57:01 -0400

    apiroutes.js now finished.

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -56,4 +56,40 @@ module.exports = function (app) {
         })
         res.send('Website scraped!')
     })
+
+
+    app.get('/api/articles/:id', function (req, res) {
+
+
+        db.Article.findOne({ _id: req.params.id })
+            .populate('note')
+            .then(function (dbArticle) {
+                res.json(dbArticle)
+            })
+            .catch(function (err) {
+                res.json(err)
+            })
+    })
+
+
+    app.post('/api/articles/:id', function (req, res) {
+
+
+        db.Note.create(req.body)
+            .then(function (dbNote) {
+
+
+                return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true })
+            })
+            .then(function (dbArticle) {
+
+
+                res.json(dbArticle)
+            })
+            .catch(function (err) {
+
+                
+                res.json(err)
+            })
+    })
 }
